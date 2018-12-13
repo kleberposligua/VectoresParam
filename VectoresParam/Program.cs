634f06f7a40c91c982ce12aa3b9a0543d5813207
kleber <kleberposligua@gmail.com>
2018-12-13 08:58:53 -0500

    Método que ordena enteros

--- VectoresParam/Program.cs
+++ VectoresParam/Program.cs
@@ -12,24 +12,61 @@ namespace VectoresParam
         static void Main(string[] args)
         {
             int[] x = new int[MAX];
+            string[] nombres = new string[MAX];
+
             leer(x);
             imprimir(x);
+
+            ordenar(x);
+            Console.WriteLine("\nVector ordenado");
+            imprimir(x);
+
             if (buscar(x, 15))
                 Console.WriteLine("\n15 encontrado");
             else
                 Console.WriteLine("\n15 NO EXISTE");
 
+            //TRABAJANDO CON EL VECTOR DE CADENAS DE CARACTERES
+            Console.WriteLine("\nINGRESE LOS NOMBRES");
+            leerNombres(nombres);
+            imprimirNombres(nombres);
+
             Console.ReadKey();
         }
 
-        public static void leer( int[] x)
+        public static void ordenar(int []x)
+        {
+            
+            
+            int aux = 0;
+            for(int i=0; i< x.Length; i++)
+               for(int j=i+1; j < (x.Length); j++)
+                  if(x[i]>x[j])
+                    {//intercambiar elementos del vector
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }
+        }
+
+        public static void leerNombres( string[] x)
         {
             for(int i=0; i<MAX; i++)
             {
                 Console.WriteLine("\nIngresa el elemento {0}",i+1);
-                x[i] =Int32.Parse(Console.ReadLine());
+                x[i] =Console.ReadLine();
             }
         }
+
+        public static void leer(int[] x)
+        {
+            for (int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine("\nIngresa el elemento {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+            }
+        }
+
         public static void imprimir(int[] x)
         {
             foreach(int num in x)
@@ -37,6 +74,15 @@ namespace VectoresParam
                 Console.WriteLine("\nElemento {0} ",num);
             }
         }
+
+        public static void imprimirNombres(string[] x)
+        {
+            foreach (string num in x)
+            {
+                Console.WriteLine("\nElemento {0} ", num);
+            }
+        }
+
         public static bool buscar(int[] x, int y)
         {
             bool encontrado = false;
